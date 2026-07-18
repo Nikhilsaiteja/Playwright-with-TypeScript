7dfd6cb6004727cb8ae1036b4333430521df082a
nikhil <nikhilsaiteja0004@gmail.com>
2026-07-18 10:11:47 +0530

    basic login flow

--- tests/login_page/loginPage.spec.ts
+++ tests/login_page/loginPage.spec.ts
@@ -1,11 +1,24 @@
-import {test,expect} from '@playwright/test';
-import { Timeout } from '../../playwright.config';
+import {test} from '@playwright/test';
+import { TestData } from '../../playwright.config';
+import { LoginPage } from './loginPage';
 
-test.beforeEach(async ({page})=> {
-    await page.goto('/');
-    await page.waitForLoadState('networkidle', {timeout: Timeout.long});
-});
+test.describe('Login Page Tests', ()=>{
 
-test.afterEach(async ({page})=> {
-    await page.close();
+    let loginPage: LoginPage;
+
+    test.beforeEach(async ({page})=>{
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
+    })
+
+    test('Verify successful login with valid credentials', async ()=>{
+        await loginPage.login(TestData.validUserName, TestData.validPassword);
+        await loginPage.expectSuccessfulLogin();
+    });
+
+    test('Verify error message with invalid credentials', async ()=>{
+        await loginPage.login(TestData.invalidUserName, TestData.invalidPassword);
+        await loginPage.expectErrorMessage();
+    });
+
 });
